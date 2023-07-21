ba283196fce2673d972ca88629f80be73a66f844
Sina Nikkhah <sina.nikkhah@livongo.com>
2023-07-21 18:23:56 -0500

    refined AI prompt msg, added regex check and return 400

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 import os
-import random
+import re
 import openai
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def parse_cpt_codes(cpt_codes):
+    keys = range(len(cpt_codes))
+    for i in keys:
+        raw = cpt_codes[i].strip()
+        print(raw)
+        print(extract_cpt(raw))
+
+def extract_cpt(string):
+    regex = "^[a-zA-Z0-9]{5}$"
+    split_array = string.split(" ")
+    keys = range(len(split_array))
+    for i in keys:
+        if (re.match(regex, split_array[i])):
+            return split_array[i]
+
+def is_cpt(string):
+    regex = "^[a-zA-Z0-9]{5}$"
+    if re.match(regex, string):
+        return True
+    else:
+        return False
 
 def chatgpt_request(prompt):
    response = openai.Completion.create(
@@ -26,15 +48,19 @@ def chatgpt_request(prompt):
 def index():
   if request.method == "POST":
     description = request.form.get("description")
-    find_cpt_prompt = f"List 3 CPT codes for the following medical procedure: {description}"
+    find_cpt_prompt = f"List 3 CPT codes for the following medical procedure in format of cpt code1, cpt code 2, cpt code 3: {description}"
     find_cpt_response = chatgpt_request(find_cpt_prompt)
     cpt_codes = find_cpt_response.strip().split(',')
+    # parse_cpt_codes(cpt_codes)
     keys = range(len(cpt_codes))
     results = []
     for i in keys:
-      print(cpt_codes[i])
+      print(cpt_codes[i].strip())
+      if not is_cpt(cpt_codes[i].strip()):
+          return "result not found", 400
+
       result = {}
-      result["code"] = cpt_codes[i]
+      result["code"] = cpt_codes[i].strip()
       cpt_desc_prompt = f"give me a description for this cpt code: {cpt_codes[i]}"
       cpt_desc_response = chatgpt_request(cpt_desc_prompt)
       result["description"] = cpt_desc_response.strip()
